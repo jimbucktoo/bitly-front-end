cc6b94abb0304286d58a87162315866578e8e725
jimbucktoo <jamesliang.g@gmail.com>
2019-04-05 14:50:03 -0600

    completion of link shortening feature component

--- src/App.js
+++ src/App.js
@@ -3,25 +3,44 @@ import Navbar from './components/Navbar';
 import LinkInput from './components/LinkInput';
 import bgImage from './assets/svgs/background-image.png';
 import './style/App.css';
+import _ from 'lodash';
 import BitlySDK from './sdk.js';
 
-const bitlySDK = new BitlySDK({
-    accessToken: ''
-});
-
-bitlySDK.shorten('http://google.com/').then(function(result){
-    console.log(result);
-});
-
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+            res: {}
+        };
+
+        this.linkShorten = this.linkShorten.bind(this)
+    }
+
+    async linkShorten(longUrl) {
+
+        const bitlySDK = new BitlySDK({
+            accessToken: '42ba4c1b14aef60604b62a2bc92591cf2f5a5355'
+        });
+
+        const results = await bitlySDK.shorten(longUrl).then(function(result){
+            return result;
+        });
+
+        this.setState({res: results})
+        console.log(this.state.res)
+    };
+
     render() {
+        const link = _.debounce((longUrl) => {this.linkShorten(longUrl)}, 300)
         return (
             <div className="App">
                 <div className='background-image' style ={ { backgroundImage: "url("+bgImage+")" } }>
                     <Navbar />
-                    <h1 class="title">SHORTEN. SHARE. MEASURE.</h1>
+                    <h1 class="title"><b>SHORTEN. SHARE. MEASURE.</b></h1>
                     <h6 class="subtitle">Join Bitly, the world's leading link management platform.</h6>
-                    <LinkInput />
+                    <LinkInput onInputLinkSubmit={link}/>
                 </div>
             </div>
         );
--- src/components/LinkInput.js
+++ src/components/LinkInput.js
@@ -2,18 +2,33 @@ import React, { Component } from 'react';
 import '../style/App.css';
 
 class LinkInput extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { longUrl: 'longUrl' };
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleSubmit(event) {
+        this.props.onInputLinkSubmit(this.state.longUrl);
+    }
+
     render() {
         return (
             <div className="uiLinkInput LinkInput">
                 <div class="uiInput input-group input-group-lg">
-                    <input type="text" class="uiInputBox form-control" placeholder="Paste a link to shorten it" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg"></input>
-                    <div class="input-group-append">
-                        <button class="btn uiButton" type="button" id="inputGroupFileAddon04">SHORTEN</button>
-                    </div>
-                </div>
+                    <input type="text" class="uiInputBox form-control form-group" placeholder="Paste a link to shorten it" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" onChange={event => this.onInputChange(event.target.value)}></input>
+    <div class="input-group-append">
+        <button class="btn uiButton" onClick={this.handleSubmit} type="submit" id="inputGroupFileAddon04">SHORTEN</button>
+    </div>
+</div>
             </div>
-        );
-    }
-}
+            );
+            }
 
-export default LinkInput;
+            onInputChange(longUrl) {
+                this.setState({longUrl});
+            };
+            }
+
+            export default LinkInput;
